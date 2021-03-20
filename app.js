ceb68a027f1fb00e4eed0598804d2d703bcb161a
nishad <nishadmm5461@gmail.com>
2021-03-21 01:43:24 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const video = document.getElementById("video"),
+  button = document.querySelector(".btn"),
+  canvas = document.getElementById("canvas"),
+  context = canvas.getContext("2d")
+
+
+navigator.getUserMedia(
+  { video: true },
+  stream => video.srcObject = stream,
+  err => console.error(err)
+)
+
+button.addEventListener("click", () => {
+  context.drawImage(video, 0, 0, 480, 320)
+})
+
